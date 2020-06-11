2e77b14d4de5168d89ea955df422f690735a35ee
AttaQureshi <attatalhaqureshi@gmail.com>
2020-06-12 01:26:17 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Dinner from './Dinner.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Dinner fruitName="Apple" vegName="Onion"/>
+        <Dinner fruitName="Banana"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Dinner(props)
+{
+    return(
+        <h1> Hi I'm Atta. I eat {props.fruitName}.</h1>
+    )
+}
+
+export default Dinner;
